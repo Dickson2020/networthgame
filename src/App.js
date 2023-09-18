1c1c74aa0aac3de19a9d65259de596dc306ebdd1
davidadeola <davidolorunsesan15@gmail.com>
2023-09-19 00:55:07 +0100

    Initialize simple react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col gap-8 items-center justify-center">
+      <h1>Dynamics App</h1>
+      <button className="text-lg font-semibold px-4 py-3 bg-blue-700 rounded-lg text-white hover:bg-blue-600">
+        Join Dynamics
+      </button>
     </div>
   );
 }
