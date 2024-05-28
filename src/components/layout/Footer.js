dedbe4dbc5d759f8192b8164b21c6b9ff014f790
Miracle Adah <75673864+Dickson2020@users.noreply.github.com>
2024-05-28 13:17:47 +0100

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="p-4 bottom-0 mt-auto border-t lg:mt-10 border-white/10 pt-8">
       <p className="text-xs leading-5 text-gray-400">
-        &copy; 2023 Dynamicxyz Demo App, David Adeola.
+        &copy; 2024 Networth Game by Dickson.
       </p>
     </footer>
   );
